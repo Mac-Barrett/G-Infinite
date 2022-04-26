6c2fed323eeedd6322ca341643b6b8d65595fa8b
Mac Barrett <maccabee.barrett3@gmail.com>
2022-04-26 13:37:25 -0500

    Increased functionality of player HUD & trackloader

--- Scripts/LevelLoader.gd
+++ Scripts/LevelLoader.gd
@@ -1,11 +1,12 @@
 extends Spatial
 
 const gridSize = 60;
-
+var startBlock
 
 func load_racers():
     # Load player car data assigned by UI Menu
-    var player = load("res://Objects/PlayerCar.tscn").instance()
+    var player = load("res://Vehicles/PlayerCar.tscn").instance()
+    player.rotation.y = deg2rad(180)
     add_child(player)
     pass
 
@@ -13,7 +14,7 @@ func load_racers():
 
 func load_track_data():
     # Load from file assigned by UI Menu
-    var data = [[1,1,1],[0,0,0],[1,1,1]]
+    var data = [[11,2,12],[0,-1,1],[10,2,13]]
     return data
 
 
@@ -21,25 +22,38 @@ func load_track_data():
 func load_track_pieces(data):
     for x in range(0, 3):
         for z in range(0, 3):
-            var trackPiece = null
-            # INSTEAD OF SWITCH:
-            #   all finished roadpieces will simply have a filename
-            #   of a number from 0 - 128 and will be loaded by adding the number
-            #   from the data array that was parsed to the path
-            match(data[x][z]):
-                0:
-                    trackPiece = load("res://Objects/Road1.tscn").instance()
-                1:
-                    trackPiece = load("res://Objects/Turn1.tscn").instance()
-            trackPiece.transform.origin.x = float(gridSize * x)
-            trackPiece.transform.origin.z = float(gridSize * z)
-            add_child(trackPiece)
+            # Load Piece from memory
+            var trackPiece = load_piece(data[x][z])
+            # If piece is not null, set transform
+            if (trackPiece != null):
+                trackPiece.transform.origin.x = float(gridSize * x)
+                trackPiece.transform.origin.z = float(gridSize * z)
+                add_child(trackPiece)
     pass
+
+
+# Loads piece from memory, returns null if tpID is -1
+func load_piece(tpID):
+    var trackPiece = null
+    match(tpID):
+        0:
+            trackPiece = load("res://TrackPieces/0.tscn").instance()
+            startBlock = trackPiece
+        1, 2:
+            trackPiece = load("res://TrackPieces/1.tscn").instance()
+            if (tpID == 2):
+                trackPiece.rotation.y = deg2rad(90)
+        10, 11, 12, 13:
+            trackPiece = load("res://TrackPieces/10.tscn").instance()
+            trackPiece.rotation.y = deg2rad(90 * (tpID - 10))
+        -1:
+            return trackPiece
+    return trackPiece
 
 
 
 func _ready():
-    load_racers()
     var trackData = load_track_data()
     load_track_pieces(trackData)
+    load_racers()
     pass
--- Scripts/PlayerCar.gd
+++ Scripts/PlayerCar.gd
@@ -1,10 +1,14 @@
 extends KinematicBody
 
-onready var HUD = get_node("HUD/Debug")
-onready var hitbox = get_node("CollisionShape")
-onready var body = get_node("Model")
-onready var camera = get_node("Camera")
+# HUD elements
+onready var DEBUG = get_node("HUD/Debug")
+onready var lapCounterLabel = get_node("HUD/TopRightBox/LapCounter")
+onready var powerSlider = get_node("HUD/TopRightBox/VBoxTopRight/Power")
+onready var healthSlider = get_node("HUD/TopRightBox/VBoxTopRight/Health")
+onready var speedLabel = get_node("HUD/SpeedBox/Speed")
+onready var speedSlider = get_node("HUD/SpeedBox/SpeedSlider")
 
+# Physics variables
 var velocity = Vector3.ZERO
 var impulse = Vector3.ZERO
 var speed = 0
@@ -17,6 +21,9 @@ var brake = 0.3
 
 var MAX_SPEED = 40
 var fps = 60
+
+var currLap = 1
+var totalLaps = 3
 
 
 # On Ready
@@ -44,7 +51,7 @@ func get_input():
 
     if not (Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_A)):
         rotation_degrees.x = lerp(rotation_degrees.x, 0, 0.1)
-        camera.rotation_degrees.z = lerp(camera.rotation_degrees.z, 0, 0.1)
+        $Camera.rotation_degrees.z = lerp($Camera.rotation_degrees.z, 0, 0.1)
     else:
         if Input.is_key_pressed(KEY_D):
             turn(true)
@@ -60,29 +67,42 @@ func turn(turnRight):
     if (turnRight == true):
         rotate_y(-turnSpeed)
         rotation_degrees.x = lerp(rotation_degrees.x, -10, 0.1)
-        camera.rotation_degrees.z = lerp(camera.rotation_degrees.z, 9, 0.1)
+        $Camera.rotation_degrees.z = lerp($Camera.rotation_degrees.z, 9, 0.1)
     else:
         rotate_y(turnSpeed)
         rotation_degrees.x = lerp(rotation_degrees.x, 10, 0.1)
-        camera.rotation_degrees.z = lerp(camera.rotation_degrees.z, -9, 0.1)
+        $Camera.rotation_degrees.z = lerp($Camera.rotation_degrees.z, -9, 0.1)
+    pass
+
+
+# updates debug HUD elements
+func update_Debug():
+    DEBUG.get_child(0).text = "Speed: " + String(speed)
+    DEBUG.get_child(1).text = "Direction: " + String(transform.basis.x)
+    DEBUG.get_child(2).text = "Velocity: " + String(velocity)
+    DEBUG.get_child(3).text = "ImpulseV: " + String(impulse)
     pass
 
 
 # updates HUD elements
 func update_HUD():
-    HUD.get_child(0).text = "Speed: " + String(speed)
-    HUD.get_child(1).text = "Velocity: " + String(transform.basis.x)
+    update_Debug()
+    lapCounterLabel.text = "Lap " + String(currLap) + "/" + String(totalLaps)
+    powerSlider.value = 100
+    healthSlider.value = 100
+    speedLabel.text = String(speed * 26) + " kmh"
+    speedSlider.value = (speed / MAX_SPEED) * 100
     pass
 
 
 # sets camera position based on speed
 func set_camera():
-    camera.translation.x = 3 + (speed / 50)
-    camera.rotation_degrees.x = -30 + (speed / 6)
+    $Camera.translation.x = 3 + (speed / 50)
+    $Camera.rotation_degrees.x = -30 + (speed / 6)
     pass
 
 
-# handles collisions
+# handles collisions from move_and_collide()
 func handle_(collision):
     if (collision != null):
         var hitMagnitude = (abs(speed) / 5) * 10
